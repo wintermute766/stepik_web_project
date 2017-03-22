102e3fd944e8f0a74531e02852173a5f59fd76b7
Eugene K <player766@gmail.com>
2017-03-22 13:53:10 +0000

    Revert "init"
    This reverts commit 995df01e5e6c8f2ac702bb9a413bdfe0d2d72921.

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#nginx conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn2.conf /etc/gunicorn.d/test2
+
+#gunicorn conf
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
