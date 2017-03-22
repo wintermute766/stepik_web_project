9a8329a46a80e7d87e4131c3af037e0bf65b73a3
Eugene K <player766@gmail.com>
2017-03-22 12:33:53 +0000

    nginx, init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #nginx conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
